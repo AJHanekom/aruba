81ccc96cdcb743211ae8455e1d6c47d7c5ba3bd4
ajhanekom <ajhanekomrsa@gmail.com>
2019-03-02 15:32:04 +0200

    AGM integration and map logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from "@angular/core";
 import { CustomerService } from "./services/customer.service";
+import { point, distance, center } from "@turf/turf";
+import { forEach } from "@angular/router/src/utils/collection";
 
 @Component({
   selector: "app-root",
@@ -9,24 +11,57 @@ import { CustomerService } from "./services/customer.service";
 export class AppComponent {
   customerList: Customer[];
 
-  lat: number = 51.678418;
-  lng: number = 7.809007;
+  lat: number = -33.9328;
+  lng: number = 18.417189;
+  radius: number = 10000;
+  eventIcon: any;
+  userIcon: any;
 
   constructor(public customerService: CustomerService) {
+    this.eventIcon = {
+      url: "../../assets/event.svg",
+      scaledSize: { height: 80, width: 80 }
+    };
+    this.userIcon = {
+      url: "../../assets/user.svg",
+      scaledSize: { height: 40, width: 40 }
+    };
+    this.refreshList();
+    this.refreshDistance();
+  }
+
+  searchInput(event: any) {
+    this.refreshList();
+    this.customerList = this.customerList.filter(
+      customer =>
+        customer.Firstname.includes(event.target.value.trim()) ||
+        customer.Surname.includes(event.target.value.trim())
+    );
+  }
+
+  refreshList() {
     this.customerService.customerList.subscribe(
       list => (this.customerList = list)
     );
   }
 
-  searchInput(event: any) {
-    this.customerService.customerList.subscribe(
-      list =>
-        (this.customerList = list.filter(
-          customer =>
-            customer.Firstname.includes(event.target.value) ||
-            customer.Surname.includes(event.target.value)
-        ))
-    );
+  refreshCircle(event) {
+    this.lat = event.lat;
+    this.lng = event.lng;
+    this.refreshDistance();
+  }
+  updateCircleRadius(event) {
+    this.radius = parseFloat(event);
+    this.refreshDistance();
+  }
+
+  refreshDistance() {
+    for (let customer of this.customerList) {
+      customer.Distance = distance(
+        point([this.lat, this.lng]),
+        point([parseFloat(customer.Lat), parseFloat(customer.Long)])
+      );
+    }
   }
 }
 
@@ -35,4 +70,5 @@ export interface Customer {
   Surname: string;
   Lat: string;
   Long: string;
+  Distance: number;
 }
